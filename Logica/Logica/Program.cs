7af74765d6c51a28e5c97a5c2a0f5ee9d2f93c24
SI51 <vayne_lala@yahoo.com>
2017-04-04 14:35:28 -0700

    Comentario

--- Logica/Logica/Program.cs
+++ Logica/Logica/Program.cs
@@ -16,7 +16,7 @@ namespace Logica
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
         }
     }
 }
